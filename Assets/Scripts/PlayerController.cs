e76d4dd6d433065f4e63f81a6012f8ace4cfe848
Neelesh <neeldudeepic990@gmail.com>
2020-12-23 12:36:47 -0600

    Added Player Script
    Added Player Script to move player and control player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float forwardSpeed = 1000f;
+    public float moveSpeed = 1500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.forward * forwardSpeed * Time.deltaTime, ForceMode.Acceleration);
+        rb.AddForce(transform.right * Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, ForceMode.Acceleration);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
